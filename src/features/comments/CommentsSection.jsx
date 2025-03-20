f73825d22763ae7b8f6dd9592d0fe888998653a7
Ethan Carriger <ethancarriger@gmail.com>
2025-03-20 18:24:26 -0400

    add routing and pages

---
+++ src/features/comments/CommentsSection.jsx
@@ -0,0 +1,7 @@
+
+const CommentsSection = () => {
+    return(
+        <h2>CommentsSection</h2>
+    );
+}
+export default CommentsSection;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import PostPage from './features/post/PostPage.jsx'
+import HomePage from './features/home/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="/post/:postId/commentId?" element={<PostPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
