6027df6e6c65d421a040dd56f98c7090ce85fbad
Ethan Carriger <ethancarriger@gmail.com>
2025-03-21 17:45:38 -0400

    post previews retreive post data from redux

--- src/components/postPreview/PostPreview.jsx
+++ src/components/postPreview/PostPreview.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
+import { selectPost } from "../../features/posts/postsSlice";
 import CounterChips from "../counterChips/CounterChips";
 
-const PostPreview = () => {
+const PostPreview = ({postId}) => {
+    
+    const post = useSelector(selectPost(postId));
+    
     return (
         <li>
-            <h3>PostPreview</h3>
+            <h3>{post.title}</h3>
             <CounterChips />
         </li>
     );
