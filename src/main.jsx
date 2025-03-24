db56222572d922b0dac1b3037db14bcd07cbe961
Ethan Carriger <ethancarriger@gmail.com>
2025-03-24 16:26:30 -0400

    install mui

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,24 @@ import store from './store.js'
 import App from './App.jsx'
 import PostPage from './features/posts/postPage/PostPage.jsx'
 import HomePage from './features/posts/homePage/HomePage.jsx'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from './theme/theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store} >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<HomePage />} />
-            <Route path="/post/:postId/commentId?" element={<PostPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<HomePage />} />
+              <Route path="/post/:postId/commentId?" element={<PostPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 )
