552452d9ba71faa8c751e1a1506d19af81771831
Ethan Carriger <ethancarriger@gmail.com>
2025-03-26 15:20:27 -0400

    add tests for chips rendering comment and like icon

--- src/components/counterChips/CounterChips.test.js
+++ src/components/counterChips/CounterChips.test.js
@@ -1,4 +1,4 @@
-import { screen, render, getAllByTestId } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import CounterChips from './CounterChips';
 
 const renderChips = (likeCount, commentCount) => {
@@ -11,4 +11,18 @@ test('renders 2 chips', () => {
     const chips = document.querySelectorAll('.MuiChip-root');
 
     expect(chips).toHaveLength(2);
+});
+test('renders chip with ThumbsUp icon', () => {
+    renderChips(1, 2);
+
+    const chip = screen.getByTestId('ThumbUpIcon');
+
+    expect(chip).toBeInTheDocument();
+});
+test('renders chip with Comment icon', () => {
+    renderChips(1, 2);
+
+    const chip = screen.getByTestId('CommentIcon');
+
+    expect(chip).toBeInTheDocument();
 });
