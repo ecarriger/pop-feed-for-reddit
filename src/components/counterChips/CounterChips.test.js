39cde865d88af9843f259982687aaa439f7a6f11
Ethan Carriger <ethancarriger@gmail.com>
2025-03-26 15:23:37 -0400

    add tests for props rendering

--- src/components/counterChips/CounterChips.test.js
+++ src/components/counterChips/CounterChips.test.js
@@ -12,6 +12,20 @@ test('renders 2 chips', () => {
 
     expect(chips).toHaveLength(2);
 });
+test('renders chip with 10 likes when passed 10 as first prop', () => {
+    renderChips(10, 2);
+
+    const chip = screen.getByText('10');
+
+    expect(chip).toBeInTheDocument();
+});
+test('renders chip with 10 comments when passed 10 as second prop', () => {
+    renderChips(1, 10);
+
+    const chip = screen.getByText('10');
+
+    expect(chip).toBeInTheDocument();
+});
 test('renders chip with ThumbsUp icon', () => {
     renderChips(1, 2);
 
