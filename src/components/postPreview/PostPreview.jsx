1190eded5533a87b9d24e038d4e75f639bf6ac38
Ethan Carriger <ethancarriger@gmail.com>
2025-04-01 14:30:44 -0400

    impliment timestamp formatting for postpreview

--- src/components/postPreview/PostPreview.jsx
+++ src/components/postPreview/PostPreview.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from "react-redux";
 import { selectPost } from "../../features/posts/postsSlice";
 import CounterChips from "../counterChips/CounterChips";
+import { formatTimestamp } from "../../utilities/utils";
 
 const PostPreview = ({postId}) => {
     
     const post = useSelector(selectPost(postId));
-    
+    const timeStamp = Date.now() - post.created;
+    const postCreated = formatTimestamp(timeStamp);
+
     return (
         <li>
             <div className="creditContainer" >
                 <h4>r/{post.subreddit}</h4>
-                <p>{post.created}</p>
+                <p>{postCreated}</p>
             </div>
             {('preview' in post) && <img alt={`r/${post.subreddit} - ${post.title}`} />}
             <h3>{post.title}</h3>
