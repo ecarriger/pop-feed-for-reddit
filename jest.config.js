3553c0a036f6ca488c3875b5636490b11cba908a
Ethan Carriger <ethancarriger@gmail.com>
2025-04-02 10:52:24 -0400

    fix jest textencoder error

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 export default {
-    testEnvironment: 'jsdom',
+    testEnvironment: 'jest-fixed-jsdom',
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
           '<rootDir>/__mocks__/fileMock.js',
