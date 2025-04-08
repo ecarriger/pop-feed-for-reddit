58c2d52a6e8e7e50a4558f9a45eb5bdfa69fa341
Ethan Carriger <ethancarriger@gmail.com>
2025-04-08 11:26:22 -0400

    add switch statement to handle post content type

--- src/components/postPreview/PostPreview.jsx
+++ src/components/postPreview/PostPreview.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 import { selectPost } from "../../features/posts/postsSlice";
+import PostImage from "../postImage/PostImage";
+import PostVideo from "../postVideo/PostVideo";
 import CounterChips from "../counterChips/CounterChips";
 import { formatTimestamp } from "../../utilities/utils";
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Typography from "@mui/material/Typography";
@@ -14,34 +14,30 @@ const PostPreview = ({postId}) => {
     
     const post = useSelector(selectPost(postId));
     const postCreated = formatTimestamp(Date.now() - post.created);
-    const mediaPresent = ('url_overridden_by_dest' in post);
+    const postType = post.post_hint;
+    let postBody;
+    switch (postType) {
+        case 'self':
+            postBody = <Typography>{post.selftext}</Typography>;
+            break;
+        case 'image':
+            postBody = <PostImage imageUrl={post.url_overridden_by_dest} alt={`r/${post.subreddit}`} />;
+            break;
+        case 'link':
+            postBody = <Typography>{post.url_overridden_by_dest}</Typography>;
+            break;
+        case 'hosted:video':
+            postBody = <PostVideo />;
+            break;
+    }
 
     return (
         <Card>
             <CardHeader subheader={`r/${post.subreddit} • ${postCreated}`} />
             <CardContent>
                 <Typography>{post.title}</Typography>
-                {!mediaPresent && <Typography>{post.selftext}</Typography>}
+                {postBody}
             </CardContent>
-            {mediaPresent 
-            && 
-            <Box sx={{
-                backgroundImage: `url(${post.url_overridden_by_dest})`, 
-                backgroundSize: 'cover',
-                borderRadius: '32px'
-            }}>
-                <CardMedia 
-                    component='img' 
-                    image={post.url_overridden_by_dest} 
-                    height='540' 
-                    alt={`r/${post.subreddit}`} 
-                    sx={{
-                        objectFit: 'contain', 
-                        backdropFilter:'blur(32px)',
-                        borderRadius: '32px'
-                    }}
-                />
-            </Box>}
             <CardActions>
                 <CounterChips likeCount={post.ups} commentCount={post.num_comments} />
             </CardActions>
