4f2daf2568a9c18edd320d1a2b9e1998eb83e06a
Ethan Carriger <ethancarriger@gmail.com>
2025-04-15 15:36:09 -0400

    add bottom padding for post title

--- src/components/postTitle/PostTitle.jsx
+++ src/components/postTitle/PostTitle.jsx
@@ -1,7 +1,7 @@
 import Typography from '@mui/material/Typography';
 
 const PostTitle = ({title}) => {
-    return <Typography variant='h5'>{title}</Typography>
+    return <Typography variant='h5' sx={{paddingBottom: '1rem'}}>{title}</Typography>
 };
 
 export default PostTitle;
