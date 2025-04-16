2ea175563c2fadf8e893f54f3d2516511e2315ef
Ethan Carriger <ethancarriger@gmail.com>
2025-04-16 12:31:50 -0400

    hide link overflow with ellipses

--- src/components/postLink/PostLink.jsx
+++ src/components/postLink/PostLink.jsx
@@ -6,10 +6,10 @@ import Link from "@mui/material/Link";
 
 const PostLink = ({title, linkUrl, imgUrl, alt}) => {
     return (
-        <CardContent sx={{display: 'flex'}} >
-            <Box sx={{display: 'flex', flexDirection: 'column'}}>
+        <CardContent sx={{display: 'flex', gap: '1rem', justifyContent: 'space-between'}} >
+            <Box sx={{display: 'flex', flexDirection: 'column', overflow: 'hidden'}}>
                 <PostTitle title={title} />
-                <Link href={linkUrl}>{linkUrl}</Link>
+                <Link href={linkUrl} sx={{textWrap: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}} >{linkUrl}</Link>
             </Box>
             {imgUrl !== 'default' && <CardMedia image={imgUrl} component='img' alt={alt} sx={{height: 'auto', width: 'auto'}} />}
         </CardContent> 
