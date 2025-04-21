8d865ec3ea7310907e3af4742be9cc700e8eed4c
Ethan Carriger <ethancarriger@gmail.com>
2025-04-21 15:59:59 -0400

    Rework post data to be handled with useeffect for switching data source capability

--- src/features/posts/homePage/HomePage.jsx
+++ src/features/posts/homePage/HomePage.jsx
@@ -1,32 +1,41 @@
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
-import { addPost, selectAllPosts } from "../postsSlice";
+import { addPost, deleteAllPosts, selectAllPosts } from "../postsSlice";
 import { loadRedditData, loadMockRedditData } from "../../../utilities/redditApi";
 import CategoryPicker from "../../../components/categoryPicker/CategoryPicker";
 import PostPreviewList from "../../../components/postPreviewList/PostPreviewList";
 
 const HomePage = () => {
-    const posts = useSelector(selectAllPosts);
     const dispatch = useDispatch();
-
-    const loadPosts = async () => {
-        const redditData = await loadRedditData();
+    const [ redditData, setRedditData ] = useState([]);
+    useEffect(() => {
+        dispatch(deleteAllPosts());
         redditData.forEach( post => {
             dispatch(addPost(post));
         });
+    }, [redditData, dispatch]);
+
+    const posts = useSelector(selectAllPosts);
+
+    const loadPosts = async () => {
+        const data = await loadRedditData();
+        setRedditData(data)
+
     }
     const loadMockPosts = async () => {
-        const mockRedditData = await loadMockRedditData();
-        mockRedditData.forEach( post => {
-            dispatch(addPost(post));
-        });
+        const data = await loadMockRedditData();
+        setRedditData(data)
     }
 
     return(
         <>
             <h2>HomePage</h2>
             <CategoryPicker />
-            {Object.values(posts).length !== 0 ? <PostPreviewList posts={posts} /> : <><button onClick={loadPosts} >Load Live Posts</button><button onClick={loadMockPosts} >Load Mock Posts</button></>}
+            <>
+                <button onClick={loadPosts} >Load Live Posts</button>
+                <button onClick={loadMockPosts} >Load Mock Posts</button>
+            </>
+            <PostPreviewList posts={posts} /> 
         </>
     );
 }
